6c28c8191639f70be14810d378455345eef7b6ba
Silvio Donato <silvio.donato@cern.ch>
2020-06-30 11:59:32 +0200

    Enable TTTracks only in D41 or later

--- Configuration/StandardSequences/python/L1TrackTrigger_cff.py
+++ Configuration/StandardSequences/python/L1TrackTrigger_cff.py
@@ -5,8 +5,13 @@ from SimTracker.TrackTriggerAssociation.TrackTriggerAssociator_cff import *
 from L1Trigger.TrackerDTC.ProducerED_cff import *
 from L1Trigger.TrackFindingTracklet.L1HybridEmulationTracks_cff import *
 
-L1TrackTrigger=cms.Sequence(TrackTriggerClustersStubs*TrackTriggerAssociatorClustersStubs*TrackerDTCProducer*L1PromptExtendedHybridTracksWithAssociators)
-#L1TrackTrigger=cms.Sequence(TrackTriggerClustersStubs*TrackTriggerAssociatorClustersStubs*TrackerDTCProducer*L1PromptExtendedHybridTracks)
+L1TrackTrigger=cms.Sequence(TrackTriggerClustersStubs*TrackTriggerAssociatorClustersStubs*TrackerDTCProducer)
 
+# Customisation to enable TTTracks in geometry D41 and later (corresponding to phase2_hgcalV10 or later). Includes the HGCAL L1 trigger
+_tttracks_l1tracktrigger = L1TrackTrigger.copy()
+_tttracks_l1tracktrigger.add(L1PromptExtendedHybridTracksWithAssociators)
+
+from Configuration.Eras.Modifier_phase2_hgcalV10_cff import phase2_hgcalV10
+phase2_hgcalV10.toReplaceWith( L1TrackTrigger, _tttracks_l1tracktrigger )
 
 TTStubAlgorithm_official_Phase2TrackerDigi_.zMatchingPS = cms.bool(True)
